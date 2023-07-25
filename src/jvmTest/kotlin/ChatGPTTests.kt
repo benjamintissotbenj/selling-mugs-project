37cda5c600b4991b80eec48a15db8396f7e73c9f
Tissot_Ben <benjamin.tissot@live.fr>
2023-07-25 11:12:47 +0100

    Writing tests for AI Generation

---
+++ src/jvmTest/kotlin/ChatGPTTests.kt
@@ -0,0 +1,45 @@
+import com.benjtissot.sellingmugs.entities.openAI.CategoriesChatRequestParams
+import com.benjtissot.sellingmugs.entities.openAI.CustomStatusCode
+import com.benjtissot.sellingmugs.entities.openAI.GenerateCategoriesStatus
+import com.benjtissot.sellingmugs.entities.openAI.OpenAIUnavailable
+import com.benjtissot.sellingmugs.services.ImageGeneratorService
+import io.ktor.util.logging.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.fail
+import kotlin.time.Duration.Companion.seconds
+
+class ChatGPTTests : AbstractDatabaseTests() {
+
+    @Test
+    fun chatGPTGenerationTest() = runTest(timeout = 120.seconds) {
+        launch {
+            val params = CategoriesChatRequestParams(5, 2, null, false)
+            try {
+                LOG.debug("Generating categories")
+                val categoriesStatus = ImageGeneratorService.generateCategoriesAndMugs(params)
+                assert(calculateSuccessPercentage(categoriesStatus) >= 75)
+            } catch (e: OpenAIUnavailable) {
+                fail(e.message)
+            }
+        }
+    }
+
+    companion object {
+
+        private val LOG = KtorSimpleLogger("ChatGPTTests.kt")
+
+        private fun calculateSuccessPercentage(categoriesStatus : GenerateCategoriesStatus) : Int {
+            return (categoriesStatus.statuses.sumOf { calculateSuccessPercentage(it.customStatusCodes) }.toFloat() / categoriesStatus.statuses.size.toFloat() * 100f).toInt()
+        }
+
+        private fun calculateSuccessPercentage(statusCodes : List<CustomStatusCode>) : Int {
+            if (statusCodes.isEmpty()) return 0
+            return (statusCodes.filter { it.value == 200 }.size.toFloat() / statusCodes.size.toFloat() * 100f).toInt()
+        }
+
+    }
+
+
+}
